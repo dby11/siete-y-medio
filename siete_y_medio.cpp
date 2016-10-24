2183759e78c71dec829af3e1855d6e0afcca928e
DESKTOP-ECV0T4E\DerekY <derekyen0811@g.ucla.edu>
2016-10-23 19:01:46 -0700

    Fixed buffering errors

--- siete_y_medio.cpp
+++ siete_y_medio.cpp
@@ -30,10 +30,11 @@ int main() {
 		play = false;
 	cout << "How much money would you like to start with?" << endl;
 	cin >> initial_amount;
+	Player player(initial_amount);
+
 	while (play) {
 		int bet;
-		Player player(initial_amount);
-		cout << "You have $" << initial_amount << ". Enter bet: " << endl;
+		cout << "You have $" << player.get_money() << ". Enter bet: " << endl;
 		cin >> bet;
 		while (true) {
 			if (bet > (player.get_money())) {
@@ -46,15 +47,12 @@ int main() {
 		bool another_card = true;
 		bool win;
 		Hand player_hand;
-		int counter = 1;
+		int counter = 0;
 		cout << "Your cards: " << endl;
-		cout << player_hand.get_card_rank(0) << "of " << player_hand.get_card_suit(0) << endl;
+		cout << player_hand.get_card_rank(0) << " of " << player_hand.get_card_suit(0) << endl;
+		cout << "Your total is " << player_hand.get_value() << endl;
+
 		while (another_card) {
-			player_hand.add_card();
-			cout << "Your cards: " << endl;
-			
-			cout << player_hand.get_card_rank(counter) << "of " << player_hand.get_card_suit(counter) << endl;
-			cout << "Your total is " << player_hand.get_value() << endl;
 			if (player_hand.get_value() < 7.5) {
 				cout << "Do you want another card (y/n)?";
 			}
@@ -64,6 +62,7 @@ int main() {
 				another_card = false;
 				break;
 			}
+			c = '\0';
 			cin >> c;
 			if (c == 'y') {
 				another_card = true;
@@ -71,13 +70,23 @@ int main() {
 			}
 			else {
 				another_card = false;
+				break;
 			}
+			player_hand.add_card();
+			cout << "Your cards: " << endl;
+
+			cout << player_hand.get_card_rank(counter) << "of " << player_hand.get_card_suit(counter) << endl;
+			cout << "Your total is " << player_hand.get_value() << endl;
 		}
 		Hand dealer_hand;
-		int dealer_counter = 1;
+		int dealer_counter = 0;
 		cout << "Dealer cards: " << endl;
-		cout << dealer_hand.get_card_rank(0) << "of " << dealer_hand.get_card_suit(0) << endl;
+		cout << dealer_hand.get_card_rank(0) << " of " << dealer_hand.get_card_suit(0) << endl;
+		cout << "Dealer total is " << dealer_hand.get_value() << ".";
+
 		while (dealer_hand.get_value() < 5.5) {
+			
+			dealer_counter++;
 			dealer_hand.add_card();
 			cout << "Dealer cards: " << endl;
 
@@ -88,10 +97,9 @@ int main() {
 				win = true;
 				break;
 			}
-			dealer_counter++;
 		}
 		if (player_hand.get_value() < 7.5 && dealer_hand.get_value() < 7.5) {
-			if (7.5 - player_hand.get_value() < 7.5 - dealer_hand.get_value()) {
+			if ((7.5 - player_hand.get_value()) <= (7.5 - dealer_hand.get_value())) {
 				win = true;
 			} else {
 				win = false;
@@ -103,11 +111,11 @@ int main() {
 			player.add_money(bet);
 		} else {
 			cout << "You lose $" << bet << endl;
-			player.lose_money(-bet);
+			player.lose_money(bet);
 		}
 
-		cout << "You win $" << bet << endl; 
 		cout << "Would you like to keep playing ? (y/n)";
+		c = '\0';
 		cin >> c;
 		if (c == 'y')
 			play = true;
